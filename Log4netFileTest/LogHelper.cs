56daed78dea31b90d6d312fcdbff06cec1ba10fb
NDChen <625119255@qq.com>
2015-12-06 00:33:29 +0800

    首次提交CommonUtility.

---
+++ Log4netFileTest/LogHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+namespace Log4netFileTest
+{
+    public class LogHelper
+    {
+        public static void WriteLog(Type t, string msg)
+        {
+            ILog logger = LogManager.GetLogger(t);
+            logger.Error(msg);
+        }
+
+        public static void WriteLog(Type t, Exception ex)
+        {
+            ILog logger = LogManager.GetLogger(t);
+            logger.Error("Error", ex);
+        }
+    }
+}
---
+++ Log4netFileTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace Log4netFileTest
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                LogHelper.WriteLog(typeof(Program), "测试log4net");
+            }
+            catch(Exception ex)
+            {
+                LogHelper.WriteLog(typeof(Program), ex);
+            }
+            
+        }
+    }
+}
